50681ed12f89474d35d7e869f05d1bffb46ee55f
debbysun23 <deborahokafor7@gmail.com>
2022-02-03 20:52:04 +0100

    my first edit

--- test.js
+++ test.js
@@ -1,119 +1,76 @@
 var buttons = document.querySelectorAll('.main');
-var storeItems = document.querySelectorAll('#store-item');
-var width = 400;
-var difference =2;
-var interveralID = 0;
+var storeItems = document.querySelectorAll('.store-item');
+var veep = document.getElementById('store-items');
 buttons.forEach(function (button) {
+    console.log("aa", buttons);
     button.addEventListener('click', function (e) {
          
             e.preventDefault()
-            if (button.classList.contains('btn')) {
-                 document.getElementById('main-sb').style.display = "none"
-            } if (button.classList.contains('bth')) {
-                 document.getElementById('main-sb').style.display = "none"
-            }if(button.classList.contains('rty')){
-              document.getElementById('main-sb').style.display = 'block';
-            } else if(button.classList.contains('bty')) {
-                Array.from(storeItems).forEach(function(item){
-                    if(item.classList.contains('cake')) {
+            const filter = e.target.dataset.filter;
+            if (filter === 'all'){
+                storeItems.forEach(function(item){
+                    item.style.display = 'block';
+                })
+            } else if(filter === 'cakes') {
+                storeItems.forEach(function(item){
+                    if(item.classList.contains('cake')){
+                        item.style.display = 'block'
+                    } else{
+                        item.style.display = 'none'
+                    }
+                })
+            } else if(filter === 'cupcakes'){   
+                storeItems.forEach(function(item){
+                    if(item.classList.contains('cupcake')){
                         item.style.display = 'block'
                     }else{
                         item.style.display = 'none'
                     }
                 })
-            } else if(button.classList.contains('hty')) {
-                Array.from(storeItems).forEach(function(item){
-                    if(item.classList.contains('cupcake')) {
+            } else if(filter === 'sweets'){
+                storeItems.forEach(function(item){
+                    if(item.classList.contains('sweet')){
                         item.style.display = 'block'
                     }else{
                         item.style.display = 'none'
                     }
                 })
-            } else if(button.classList.contains('bk')) {
-                Array.from(storeItems).forEach(function(item){
-                    if(item.classList.contains('sweet')) {
+            } else if(filter === 'doughnuts'){
+                storeItems.forEach(function(item){
+                    if(item.classList.contains('dougnut')){
                         item.style.display = 'block'
                     }else{
                         item.style.display = 'none'
-                    }    
-                })    
-    
-            }  else if(button.classList.contains('hoy')) {
-                Array.from(storeItems).forEach(function(item){
-                    if(item.classList.contains('dougnut')) {
-                        item.style.display = 'block'
-                    }else{
-                        item.style.display = 'none'
-                    }    
-                })     
-           }          
-    });    
-});
-
-const searchBar = document.forms["fb"].querySelector("input");
-//console.log(searchBar)
-//const storeItems = document.querySelectorAll('#store-item');
-//console.log(storeItems)
-searchBar.addEventListener('keyup', function (e) {
-    const term = e.target.value.toLowerCase().trim();
-    console.log('ssssss', storeItems)
-    Array.from(storeItems).forEach(function (item) {
-       // console.log(item)
-        if (item.textContent.includes(term)) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
+                    }
+                })
+            } else if(filter === 'explore'){
+                veep.style.display = 'none'
+            }else{
+                veep.style.display = ' block'
+            }
+    }) 
+})  
+    const buttonns = document.querySelector('.rty');
+    const storeItemms = document.querySelectorAll('.store-item');
+    buttonns.addEventListener('click', function (e){
+        if(buttonns.classList.contains('big')){
+            veep.style.display = 'block'
+        }else{
+            veep.style.display = 'none'
         }
-    });
-});
-
-buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-        e.preventDefault;
-        if(button.classList.contains('rty')){
-            Array.from(storeItems).forEach(function(item){
-                item.style.display = 'block'
-            })    
-
-        }   
     })
-})
+   
+    const searchBox = document.querySelector('#form');
+    const storeItemmms = document.querySelectorAll('.store-item');
 
-/*function increase() 
-{
-    clearInterval(interveralID);
-    interveralID =setInterval(expand,20);
-}
-function decrease() 
-{
-    clearInterval(interveralID);
-    interveralID =setInterval(shrink,20);
-}
-
-function expand() 
-{
-    if(width<500)
-    {
-        width = width+difference;
-        document.getElementById("img6").style.width= width;
-        document.getElementById("rit").style.display = 'block';
-        console.log(width);
-    }
-    else
-    {
-        clearInterval(interveralID);
-    }
-}
-function shrink() 
-{
-    if(width>400)
-    {
-        width = width-difference;
-        document.getElementById("img6").style.width= width;
-       // document.getElementById("rit").style.display = 'none';
-        console.log(width);
-    }
-    else
-    {
-        clearInterval(interveralID);
-}   }*/
+    searchBox.addEventListener('keyup', (e) => {
+       const searchFilter = e.target.value.toLowerCase().trim();
+        console.log('fee', searchFilter);
+       storeItemmms.forEach((item) => {
+           if(item.textContent.includes(searchFilter)){
+               item.style.display = 'block'
+           } else{
+               item.style.display = 'none'
+           }
+       })
+    })
